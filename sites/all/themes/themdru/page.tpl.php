894389006a07f2eef43f3620d828169384d06160
Baher Rais <baher.rais@gmail.com>
2017-02-16 19:04:12 +0100

    vid-03 : add region on info file

--- sites/all/themes/themdru/page.tpl.php
+++ sites/all/themes/themdru/page.tpl.php
@@ -1,15 +1,47 @@
 <div id="page">
 
 	<header>
-		
-	<?php if ($site_name): ?>
-		<h1 id="site_name">
-		<a href="<?php print $front_page; ?>"> <spam> <?php print $site_name; ?> </span> </spam></a>
-		</h1>
-	<?php endif;?>
+		<div id="header_link">
+			<?php if ($site_name): ?>
+				<h1 id="site_name">
+				<a href="<?php print $front_page; ?>"> <spam> <?php print $site_name; ?> </span> </spam></a>
+				</h1>
+			<?php endif;?>
+
+			<div id="user_link">
+				<ul >
+	                <li>
+	                    <a href="<?php print $base_url.'/drupal/user/register'; ?>"><span class="fa fa-user-plus"></span>Sign Up</a>
+	                </li>
+	                <li>
+	                    <a href="<?php print $base_url.'/drupal/user/login'; ?>"><span class="fa fa-sign-in"></span>Login</a>
+	                </li>
+	            </ul>
+	        </div> 
+		</div>
+
 	</header>
 
 	<div id="navigation">
+		<?php if ($main_menu): ?>
+	        <nav id="main-menu" role="navigation" tabindex="-1">
+	           
+	          <?php
+	          
+	          print theme('links__system_main_menu', array(
+	            'links' => $main_menu,
+	            'attributes' => array(
+	              'class' => array('links', 'inline', 'clearfix'),
+	            ),
+	            'heading' => array(
+	              'text' => t('Main menu'),
+	              'level' => 'h2',
+	              'class' => array('element-invisible'),
+	            ),
+	          )); ?>
+	        </nav>
+      	<?php endif; ?>
+
 		
 		<?php print render($page['navigation']); ?>
 
