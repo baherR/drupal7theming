d8cc6cc7d1da4c4de1e57193f1831f2a2e9fa95f
Baher Rais <baher.rais@gmail.com>
2017-02-16 18:55:11 +0100

    vid-01  Drupal 7  Theming : Configuration and  Header creation.

---
+++ sites/all/themes/themdru/page.tpl.php
@@ -0,0 +1,12 @@
+<div id="page">
+
+	<header>
+		
+	<?php if ($site_name): ?>
+		<h1 id="site_name">
+		<a href="<?php print $front_page; ?>"> <spam> <?php print $site_name; ?> </span> </spam></a>
+		</h1>
+	<?php endif;?>
+	</header>
+	
+</div>
